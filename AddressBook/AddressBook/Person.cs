8a8c63d3d4f71609ffb78b4e920690ed57bf86ca
Your UserName <aratinaik151997@gmail.com>
2021-12-02 16:54:03 +0530

    Added create method to create contact

---
+++ AddressBook/AddressBook/Person.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBook
+{
+    public class Person
+    {
+        public string fisrtName { get; set; }
+        public string lastName { get; set; }
+        public string address { get; set; }
+        public string city { get; set; }
+        public string state { get; set; }
+        public double phoneNumber { get; set; }
+        public double zip { get; set; }
+        public string email { get; set; }
+    }
+}  
+
+    
--- AddressBook/AddressBook/Program.cs
+++ AddressBook/AddressBook/Program.cs
@@ -8,10 +8,37 @@ namespace AddressBook
 {
     class Program
     {
+        Person personobj = new Person();
+        List<Person> personlist = new List<Person>();
         static void Main(string[] args)
         {
+            Program program = new Program();
             Console.WriteLine("Welcome to Address Book program");
+            program.Create();
             Console.ReadKey();
         }
+        public void Create()//create new contact
+        {
+            Console.WriteLine("\nEnter person details ");
+            Console.Write("\nEnter First Name : ");
+            personobj.fisrtName = Console.ReadLine();
+            Console.Write("Enter Last Name : ");
+            personobj.lastName = Console.ReadLine();
+            Console.Write("Enter Address : ");
+            personobj.address = Console.ReadLine();
+            Console.Write("Enter City : ");
+            personobj.city = Console.ReadLine();
+            Console.Write("Enter State : ");
+            personobj.state = Console.ReadLine();
+            Console.Write("Enter phone number : ");
+            personobj.phoneNumber = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Enter Zip : ");
+            personobj.zip = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Enter Email : ");
+            personobj.email = Console.ReadLine();
+            personlist.Add(personobj);
+            Console.WriteLine("Contact created successfully");
+        }
+
     }
 }
