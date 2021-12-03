17e30efb24e993f2a4d1a2e39b40a7dd4e026c0c
Your UserName <aratinaik151997@gmail.com>
2021-12-03 12:49:42 +0530

    Added method to edit contact using person name

--- AddressBook/AddressBook/Program.cs
+++ AddressBook/AddressBook/Program.cs
@@ -10,12 +10,34 @@ namespace AddressBook
     {
         Person personobj = new Person();
         List<Person> personlist = new List<Person>();
+        List<Person> personlist1 = new List<Person>();
         static void Main(string[] args)
         {
             Program program = new Program();
             Console.WriteLine("Welcome to Address Book program");
-            program.Create();
-            program.Show();
+            char ans;
+            do
+            {
+                Console.WriteLine("1)Add Contact   2)Show Contact   3)Edit Contact");
+                Console.Write("Enter your choice : ");
+                int choice = Convert.ToInt32(Console.ReadLine());
+                switch (choice)
+                {
+                    case 1:
+                        program.Create();
+                        break;
+                    case 2: 
+                        program.Show();
+                        break;
+                    case 3:
+                        program.Edit();
+                        break;
+
+                }
+                Console.Write("Want to Continue(Y/N) : ");
+                ans = Convert.ToChar(Console.ReadLine());
+            } while (ans == 'Y' || ans=='y');
+                      
             Console.ReadKey();
         }
         public void Create()//create new contact
@@ -43,6 +65,13 @@ namespace AddressBook
         }
         public void Show()
         {
+            if (personlist.Count == 0)
+            {
+                Console.Write("Your address book is empty.Press 1 to add new contact : ");
+                Console.ReadKey();
+                Create();
+            }
+            else
             foreach (Person details in personlist)//showing items in list
             {
                 Console.WriteLine("Person details are :");
@@ -52,7 +81,80 @@ namespace AddressBook
                 Console.WriteLine("----------------------------------------------------------------------");
             }
         }
+        public void Edit()
+        {
+            Console.WriteLine("Enter FirstName of person which contact wants to edit");
+            string newName = Console.ReadLine();
+            char ans;
+            if(newName==personobj.fisrtName)
+            {
+                //do
+                //{
+            
+                    Console.WriteLine("1)FirstName 2)LastName 3)Address 4)City 5)State 6)Phone Number 7)Zip 8)Email");
+                    Console.Write("Enter number from above list which you wants to edit :");
+                    int choice = Convert.ToInt32(Console.ReadLine());
+                    switch (choice)
+                    {
+                        case 1:
+                            Console.Write("\nEnter First Name : ");
+                            personobj.fisrtName = Console.ReadLine();
 
+                            break;
+                        case 2:
+                            Console.Write("Enter Last Name : ");
+                            personobj.lastName = Console.ReadLine();
+                            break;
+                        case 3:
+                            Console.Write("Enter Address : ");
+                            personobj.address = Console.ReadLine();
+                            EditContact(personobj);
+                            break;
+                        case 4:
+                            Console.Write("Enter City : ");
+                            personobj.city = Console.ReadLine();
+                            EditContact(personobj);
+                            break;
+                        case 5:
+                            Console.Write("Enter State : ");
+                            personobj.state = Console.ReadLine();
+                            EditContact(personobj);
+                            break;
+                        case 6:
+                            Console.Write("Enter phone number : ");
+                            personobj.phoneNumber = Convert.ToDouble(Console.ReadLine());
+                            EditContact(personobj);
+                            break;
+                        case 7:
+                            Console.Write("Enter Zip : ");
+                            personobj.zip = Convert.ToDouble(Console.ReadLine());
+                            EditContact(personobj);
+                            break;
+                        case 8:
+                            Console.Write("Enter Email : ");
+                            personobj.email = Console.ReadLine();
+                            EditContact(personobj);
+                            break;
+                        default:
+                            Console.WriteLine("Please enter valid numbers");
+                            break;
+                    }
+                Console.WriteLine("\nContact Updated successfully");
+                Console.WriteLine("----------------------------------------------------------------------");
+
+                //    Console.Write("Want to Continue(Y/N) : ");
+                //    ans = Convert.ToChar(Console.ReadLine());
+                //} while (ans == 'Y' || ans == 'y');
+            }
+            else
+            {
+                Console.WriteLine("Contact not found");
+            }
+        }
+        public void EditContact(Person person)
+        {
+            personlist1.Add(person);
+        }
 
     }
 }
